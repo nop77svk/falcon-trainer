0f45db45437d16912ffacc19d433d479ecc238cf
pierre@audrey <nop77svk@gmail.com>
2023-10-31 23:02:24 +0100

    ➕ Win GUI now initializes possible math tasks

--- FalconTrainer.Gui.Win/MainWindow.axaml.cs
+++ FalconTrainer.Gui.Win/MainWindow.axaml.cs
@@ -1,11 +1,28 @@
-using Avalonia.Controls;
-
 namespace FalconTrainer.Gui.Win;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
+using NoP77svk.FalconTrainer.Core;
 
 public partial class MainWindow : Window
 {
+    private Dictionary<int, MathTask> _mathTasks = new Dictionary<int, MathTask>();
+
     public MainWindow()
     {
         InitializeComponent();
+
+        try
+        {
+            _mathTasks = MathTasksFactory.GenerateAllPossibleTasks(6)
+                .ToDictionary(task => task.Id);
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            // 2do!
+            throw new Exception("This should not have happened!", e);
+        }
     }
 }
